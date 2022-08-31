6226cfd8d6aee0cb22d7698dbe83e2a1f674f0f5
Oliver Meulengracht <Mollern2000@outlook.dk>
2022-08-31 14:55:09 +0200

    Update Dev.sh

--- Dev.sh
+++ Dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname "$SCRIPT"`
 
 # Dev-libraries
 echo "** installing build dependencies"
@@ -10,5 +9,5 @@ apt-get -y -qq install git gcc g++ zip unzip nasm make curl
 # Install dotnet core
 if ! [ -x "$(command -v dotnet)" ]; then
   echo "** installing dotnet core"
-  "$SCRIPTPATH"/dotnet-install.sh --channel 3.1
+  dotnet-install.sh --channel 3.1
 fi
